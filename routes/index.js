3af32690078b4bd937492cfee38c61fbf2f84f46
haidang666 <hdcompany123@gmail.com>
2017-09-05 15:13:08 +0700

    update tempate

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var baseUrl = req.baseUrl;
+	while(baseUrl.charAt(0) === '/')
+	{
+		baseUrl = baseUrl.substr(1);
+	}
+
+	res.render(baseUrl, {}, function(err, html) {
+		if(err) {
+			console.log(baseUrl);
+			console.log("View not exist");
+			res.render('index'); // File doesn't exist
+		} else {
+			console.log(baseUrl);
+			res.render(baseUrl); 
+		}
+	});
 });
 
 module.exports = router;
